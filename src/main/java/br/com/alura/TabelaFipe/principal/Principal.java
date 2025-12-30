cd62a23cc59fe126a5582449523c09ef2333ae1a
livia-pimentel <livia_macedo89@yahoo.com.br>
2025-12-30 17:45:14 -0300

    Add classe para exibir o menu e fazer requisição da API

---
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -0,0 +1,43 @@
+package br.com.alura.TabelaFipe.principal;
+
+import br.com.alura.TabelaFipe.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+    // Atributo
+    private Scanner leitura = new Scanner(System.in);
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+    private ConsumoApi requisicaoApi = new ConsumoApi();
+
+    // Metodo para exibir o menu
+    public void exibeMenu() {
+
+
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consultar:
+                """;
+
+        System.out.println(menu);
+        var opcao = leitura.nextLine();
+
+        String endereco;
+
+        if (opcao.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "moto/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = requisicaoApi.obterDados(endereco);
+        System.out.println(json);
+    }
+
+}
